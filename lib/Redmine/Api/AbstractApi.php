e8b5aab5f4eb302d7d338103a1eac801ae38107f
Jan Malte Gerth <anmeldungen@malte-gerth.de>
2014-05-20 15:30:08 +0200

    added skip of new iteration if no limit can set
    If the response don't has the "limit" key, no limit can be set and therefor we don't need a further iteration. Maybe the complete part should be refactored some day.

--- lib/Redmine/Api/AbstractApi.php
+++ lib/Redmine/Api/AbstractApi.php
@@ -123,7 +123,7 @@ abstract class AbstractApi
             $ret = array_merge_recursive($ret, $newDataSet);
 
             $offset += $_limit;
-            if (empty($newDataSet) || (
+            if (empty($newDataSet) || !array_key_exists('limit', $newDataSet) || (
                     isset($newDataSet['offset']) &&
                     isset($newDataSet['total_count']) &&
                     $newDataSet['offset'] >= $newDataSet['total_count']
