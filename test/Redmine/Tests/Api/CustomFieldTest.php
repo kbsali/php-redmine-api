b464702347e5e0ca9047bae46d1b5a5ae25ae583
Malte Gerth <support@malte-gerth.de>
2014-05-31 08:00:36 +0200

    added unit tests for custom fields

--- test/Redmine/Tests/Api/CustomFieldTest.php
+++ test/Redmine/Tests/Api/CustomFieldTest.php
@@ -129,4 +129,162 @@ class CustomFieldTest extends \PHPUnit_Framework_TestCase
         // Perform the tests
         $this->assertSame(array($getResponse), $api->all($allParameters));
     }
+
+    /**
+     * Test listing()
+     *
+     * @covers ::listing
+     * @test
+     *
+     * @return void
+     */
+    public function testListingReturnsNameIdArray()
+    {
+        // Test values
+        $getResponse = array(
+            'custom_fields' => array(
+                array('id' => 1, 'name' => 'CustomField 1'),
+                array('id' => 5, 'name' => 'CustomField 5')
+            ),
+        );
+        $expectedReturn = array(
+            'CustomField 1' => 1,
+            'CustomField 5' => 5,
+        );
+
+        // Create the used mock objects
+        $client = $this->getMockBuilder('Redmine\Client')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $client->expects($this->atLeastOnce())
+            ->method('get')
+            ->with(
+                $this->stringStartsWith('/custom_fields.json')
+            )
+            ->willReturn($getResponse);
+
+        // Create the object under test
+        $api = new CustomField($client);
+
+        // Perform the tests
+        $this->assertSame($expectedReturn, $api->listing());
+    }
+
+    /**
+     * Test listing()
+     *
+     * @covers ::listing
+     * @test
+     *
+     * @return void
+     */
+    public function testListingCallsGetOnlyTheFirstTime()
+    {
+        // Test values
+        $getResponse = array(
+            'custom_fields' => array(
+                array('id' => 1, 'name' => 'CustomField 1'),
+                array('id' => 5, 'name' => 'CustomField 5')
+            ),
+        );
+        $expectedReturn = array(
+            'CustomField 1' => 1,
+            'CustomField 5' => 5,
+        );
+
+        // Create the used mock objects
+        $client = $this->getMockBuilder('Redmine\Client')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $client->expects($this->once())
+            ->method('get')
+            ->with(
+                $this->stringStartsWith('/custom_fields.json')
+            )
+            ->willReturn($getResponse);
+
+        // Create the object under test
+        $api = new CustomField($client);
+
+        // Perform the tests
+        $this->assertSame($expectedReturn, $api->listing());
+        $this->assertSame($expectedReturn, $api->listing());
+    }
+
+    /**
+     * Test listing()
+     *
+     * @covers ::listing
+     * @test
+     *
+     * @return void
+     */
+    public function testListingCallsGetEveryTimeWithForceUpdate()
+    {
+        // Test values
+        $getResponse = array(
+            'custom_fields' => array(
+                array('id' => 1, 'name' => 'CustomField 1'),
+                array('id' => 5, 'name' => 'CustomField 5')
+            ),
+        );
+        $expectedReturn = array(
+            'CustomField 1' => 1,
+            'CustomField 5' => 5,
+        );
+
+        // Create the used mock objects
+        $client = $this->getMockBuilder('Redmine\Client')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $client->expects($this->exactly(2))
+            ->method('get')
+            ->with(
+                $this->stringStartsWith('/custom_fields.json')
+            )
+            ->willReturn($getResponse);
+
+        // Create the object under test
+        $api = new CustomField($client);
+
+        // Perform the tests
+        $this->assertSame($expectedReturn, $api->listing(true));
+        $this->assertSame($expectedReturn, $api->listing(true));
+    }
+
+    /**
+     * Test getIdByName()
+     *
+     * @covers ::getIdByName
+     * @test
+     *
+     * @return void
+     */
+    public function testGetIdByNameMakesGetRequest()
+    {
+        // Test values
+        $getResponse = array(
+            'custom_fields' => array(
+                array('id' => 5, 'name' => 'CustomField 5')
+            ),
+        );
+
+        // Create the used mock objects
+        $client = $this->getMockBuilder('Redmine\Client')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $client->expects($this->once())
+            ->method('get')
+            ->with(
+                $this->stringStartsWith('/custom_fields.json')
+            )
+            ->willReturn($getResponse);
+
+        // Create the object under test
+        $api = new CustomField($client);
+
+        // Perform the tests
+        $this->assertFalse($api->getIdByName('CustomField 1'));
+        $this->assertSame(5, $api->getIdByName('CustomField 5'));
+    }
 }
