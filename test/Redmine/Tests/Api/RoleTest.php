252f615897d41ae4fb246f7528319785e945e9b0
Malte Gerth <support@malte-gerth.de>
2014-06-01 13:11:46 +0200

    added tests for role API

---
+++ test/Redmine/Tests/Api/RoleTest.php
@@ -0,0 +1,249 @@
+<?php
+/**
+ * Role API test
+ *
+ * PHP version 5.4
+ *
+ * @author     Malte Gerth <mail@malte-gerth.de>
+ * @copyright  2014 Malte Gerth
+ * @license    MIT
+ * @link       https://github.com/kbsali/php-redmine-api
+ * @since      2014-06-01
+ */
+
+namespace Redmine\Tests\Api;
+
+use Redmine\Api\Role;
+
+/**
+ * Role API test
+ *
+ * @coversDefaultClass Redmine\Api\Role
+ *
+ * @author     Malte Gerth <mail@malte-gerth.de>
+ * @copyright  2014 Malte Gerth
+ * @license    MIT
+ * @link       https://github.com/kbsali/php-redmine-api
+ * @since      2014-06-01
+ */
+class RoleTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * Test all()
+     *
+     * @covers ::all
+     * @test
+     *
+     * @return void
+     */
+    public function testAllReturnsClientGetResponse()
+    {
+        // Test values
+        $getResponse = 'API Response';
+
+        // Create the used mock objects
+        $client = $this->getMockBuilder('Redmine\Client')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $client->expects($this->once())
+            ->method('get')
+            ->with(
+                $this->stringStartsWith('/roles.json')
+            )
+            ->willReturn($getResponse);
+
+        // Create the object under test
+        $api = new Role($client);
+
+        // Perform the tests
+        $this->assertSame($getResponse, $api->all());
+    }
+
+    /**
+     * Test all()
+     *
+     * @covers ::all
+     * @test
+     *
+     * @return void
+     */
+    public function testAllReturnsClientGetResponseWithParametersAndProject()
+    {
+        // Test values
+        $parameters = array('not-used');
+        $getResponse = array('API Response');
+
+        // Create the used mock objects
+        $client = $this->getMockBuilder('Redmine\Client')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $client->expects($this->any())
+            ->method('get')
+            ->with(
+                $this->logicalAnd(
+                    $this->stringStartsWith('/roles.json'),
+                    $this->stringContains('not-used')
+                )
+            )
+            ->willReturn($getResponse);
+
+        // Create the object under test
+        $api = new Role($client);
+
+        // Perform the tests
+        $this->assertSame($getResponse, $api->all($parameters));
+    }
+
+    /**
+     * Test listing()
+     *
+     * @covers ::listing
+     * @test
+     *
+     * @return void
+     */
+    public function testListingReturnsNameIdArray()
+    {
+        // Test values
+        $getResponse = array(
+            'roles' => array(
+                array('id' => 1, 'name' => 'Role 1'),
+                array('id' => 5, 'name' => 'Role 5')
+            ),
+        );
+        $expectedReturn = array(
+            'Role 1' => 1,
+            'Role 5' => 5,
+        );
+
+        // Create the used mock objects
+        $client = $this->getMockBuilder('Redmine\Client')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $client->expects($this->atLeastOnce())
+            ->method('get')
+            ->with(
+                $this->stringStartsWith('/roles.json')
+            )
+            ->willReturn($getResponse);
+
+        // Create the object under test
+        $api = new Role($client);
+
+        // Perform the tests
+        $this->assertSame($expectedReturn, $api->listing());
+    }
+
+    /**
+     * Test listing()
+     *
+     * @covers ::listing
+     * @test
+     *
+     * @return void
+     */
+    public function testListingCallsGetOnlyTheFirstTime()
+    {
+        // Test values
+        $getResponse = array(
+            'roles' => array(
+                array('id' => 1, 'name' => 'Role 1'),
+                array('id' => 5, 'name' => 'Role 5')
+            ),
+        );
+        $expectedReturn = array(
+            'Role 1' => 1,
+            'Role 5' => 5,
+        );
+
+        // Create the used mock objects
+        $client = $this->getMockBuilder('Redmine\Client')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $client->expects($this->once())
+            ->method('get')
+            ->with(
+                $this->stringStartsWith('/roles.json')
+            )
+            ->willReturn($getResponse);
+
+        // Create the object under test
+        $api = new Role($client);
+
+        // Perform the tests
+        $this->assertSame($expectedReturn, $api->listing());
+        $this->assertSame($expectedReturn, $api->listing());
+    }
+
+    /**
+     * Test listing()
+     *
+     * @covers ::listing
+     * @test
+     *
+     * @return void
+     */
+    public function testListingCallsGetEveryTimeWithForceUpdate()
+    {
+        // Test values
+        $getResponse = array(
+            'roles' => array(
+                array('id' => 1, 'name' => 'Role 1'),
+                array('id' => 5, 'name' => 'Role 5')
+            ),
+        );
+        $expectedReturn = array(
+            'Role 1' => 1,
+            'Role 5' => 5,
+        );
+
+        // Create the used mock objects
+        $client = $this->getMockBuilder('Redmine\Client')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $client->expects($this->exactly(2))
+            ->method('get')
+            ->with(
+                $this->stringStartsWith('/roles.json')
+            )
+            ->willReturn($getResponse);
+
+        // Create the object under test
+        $api = new Role($client);
+
+        // Perform the tests
+        $this->assertSame($expectedReturn, $api->listing(true));
+        $this->assertSame($expectedReturn, $api->listing(true));
+    }
+
+    /**
+     * Test show()
+     *
+     * @covers ::get
+     * @covers ::show
+     * @test
+     *
+     * @return void
+     */
+    public function testShowReturnsClientGetResponse()
+    {
+        // Test values
+        $getResponse = 'API Response';
+
+        // Create the used mock objects
+        $client = $this->getMockBuilder('Redmine\Client')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $client->expects($this->once())
+            ->method('get')
+            ->with($this->stringStartsWith('/roles/5.json'))
+            ->willReturn($getResponse);
+
+        // Create the object under test
+        $api = new Role($client);
+
+        // Perform the tests
+        $this->assertSame($getResponse, $api->show(5));
+    }
+}
