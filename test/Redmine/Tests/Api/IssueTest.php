0167238caa9dffac72502428de455095a598478b
Malte Gerth <support@malte-gerth.de>
2014-06-10 06:38:29 +0200

    added test for cleanParams and create of issue API

--- test/Redmine/Tests/Api/IssueTest.php
+++ test/Redmine/Tests/Api/IssueTest.php
@@ -30,6 +30,22 @@ class IssueTest extends \PHPUnit_Framework_TestCase
 {
 
     /**
+     * Test the constants
+     *
+     * @test
+     *
+     * @return void
+     */
+    public function testPriorityConstants()
+    {
+        $this->assertSame(1, Issue::PRIO_LOW);
+        $this->assertSame(2, Issue::PRIO_NORMAL);
+        $this->assertSame(3, Issue::PRIO_HIGH);
+        $this->assertSame(4, Issue::PRIO_URGENT);
+        $this->assertSame(5, Issue::PRIO_IMMEDIATE);
+    }
+
+    /**
      * Test all()
      *
      * @covers ::all
@@ -311,5 +327,114 @@ class IssueTest extends \PHPUnit_Framework_TestCase
 
         // Perform the tests
         $this->assertSame($getResponse, $api->removeWatcher(5, 10));
+    }
+
+    /**
+     * Test create()
+     *
+     * @covers ::create
+     * @covers ::post
+     * @test
+     *
+     * @return void
+     */
+    public function testCreateCallsPost()
+    {
+        // Test values
+        $getResponse = 'API Response';
+        $parameters = array();
+
+        // Create the used mock objects
+        $client = $this->getMockBuilder('Redmine\Client')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $client->expects($this->once())
+            ->method('post')
+            ->with(
+                '/issues.xml',
+                '<?xml version="1.0"?>' . PHP_EOL . '<issue/>' . PHP_EOL
+            )
+            ->willReturn($getResponse);
+
+        // Create the object under test
+        $api = new Issue($client);
+
+        // Perform the tests
+        $this->assertSame($getResponse, $api->create($parameters));
+    }
+
+    /**
+     * Test cleanParams()
+     *
+     * @covers ::create
+     * @covers ::cleanParams
+     * @test
+     *
+     * @return void
+     */
+    public function testCreateCleansParameters()
+    {
+        // Test values
+        $getResponse = 'API Response';
+        $parameters = array(
+            'project' => 'Project Name',
+            'category' => 'Category Name',
+            'status' => 'Status Name',
+            'tracker' => 'Tracker Name',
+            'assigned_to' => 'Assigned to User Name',
+            'author' => 'Author Name',
+        );
+
+        // Create the used mock objects
+        $getIdByNameApi = $this->getMockBuilder('Redmine\Api\Project')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $getIdByNameApi->expects($this->exactly(4))
+            ->method('getIdByName')
+            ->willReturn('cleanedValue');
+        $getIdByUsernameApi = $this->getMockBuilder('Redmine\Api\User')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $getIdByUsernameApi->expects($this->exactly(2))
+            ->method('getIdByUsername')
+            ->willReturn('cleanedValue');
+
+        $client = $this->getMockBuilder('Redmine\Client')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $client->expects($this->exactly(6))
+            ->method('api')
+            ->willReturnMap(
+                array(
+                    array('project', $getIdByNameApi),
+                    array('issue_category', $getIdByNameApi),
+                    array('issue_status', $getIdByNameApi),
+                    array('tracker', $getIdByNameApi),
+                    array('user', $getIdByUsernameApi),
+                )
+            );
+
+        $client->expects($this->once())
+            ->method('post')
+            ->with(
+                '/issues.xml',
+                $this->logicalAnd(
+                    $this->stringStartsWith('<?xml version="1.0"?>' . PHP_EOL . '<issue>'),
+                    $this->stringEndsWith('</issue>' . PHP_EOL),
+                    $this->stringContains('<project_id>cleanedValue</project_id>'),
+                    $this->stringContains('<category_id>cleanedValue</category_id>'),
+                    $this->stringContains('<status_id>cleanedValue</status_id>'),
+                    $this->stringContains('<tracker_id>cleanedValue</tracker_id>'),
+                    $this->stringContains('<assigned_to_id>cleanedValue</assigned_to_id>'),
+                    $this->stringContains('<author_id>cleanedValue</author_id>')
+                )
+            )
+            ->willReturn($getResponse);
+
+        // Create the object under test
+        $api = new Issue($client);
+
+        // Perform the tests
+        $this->assertSame($getResponse, $api->create($parameters));
     }
 }
