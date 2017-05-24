c868eb5be34279cc82e16ed6b2a96d374f2cd438
Art4 <art4@wlabs.de>
2017-05-24 15:20:45 +0200

    Restore autoload for tests in autoload.php

--- lib/autoload.php
+++ lib/autoload.php
@@ -7,8 +7,19 @@ spl_autoload_register(function ($class) {
      */
     $class = str_replace('\\', '/', $class);
 
-    // Check under the current directory
+    /* First, check under the current directory. It is important that
+     * we look here first, so that we don't waste time searching for
+     * test classes in the common case.
+     */
     $path = dirname(__FILE__).'/'.$class.'.php';
+    if (file_exists($path)) {
+        require_once $path;
+    }
+
+    /* If we didn't find what we're looking for already, maybe it's
+     * a test class?
+     */
+    $path = dirname(__FILE__).'/../test/'.$class.'.php';
     if (file_exists($path)) {
         require_once $path;
     }
