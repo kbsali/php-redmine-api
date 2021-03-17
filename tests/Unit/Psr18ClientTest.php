2a4b5e1e665b8a81a32d0597b105419cb3af5444
Art4 <art4@wlabs.de>
2021-03-17 17:03:21 +0100

    Test Request created by client

--- tests/Unit/Psr18ClientTest.php
+++ tests/Unit/Psr18ClientTest.php
@@ -2,6 +2,7 @@
 
 namespace Redmine\Tests\Unit;
 
+use GuzzleHttp\Psr7\ServerRequest;
 use InvalidArgumentException;
 use PHPUnit\Framework\TestCase;
 use Psr\Http\Client\ClientInterface as HttpClient;
@@ -124,7 +125,7 @@ class Psr18ClientTest extends TestCase
             'access_token'
         );
 
-        $this->assertSame(false, $client->requestGet('path'));
+        $this->assertSame(false, $client->requestGet('/path'));
     }
 
     /**
@@ -152,7 +153,7 @@ class Psr18ClientTest extends TestCase
             'access_token'
         );
 
-        $this->assertSame(true, $client->requestGet('path'));
+        $this->assertSame(true, $client->requestGet('/path'));
         $this->assertSame(200, $client->getLastResponseStatusCode());
         $this->assertSame('application/json', $client->getLastResponseContentType());
         $this->assertSame('{"foo_bar": 12345}', $client->getLastResponseBody());
@@ -223,5 +224,80 @@ class Psr18ClientTest extends TestCase
         $this->expectExceptionMessage('`do_not_exist` is not a valid api. Possible apis are `attachment`, `group`, `custom_fields`, `issue`, `issue_category`, `issue_priority`, `issue_relation`, `issue_status`, `membership`, `news`, `project`, `query`, `role`, `time_entry`, `time_entry_activity`, `tracker`, `user`, `version`, `wiki`, `search`');
 
         $client->getApi('do_not_exist');
+    }
+
+    /**
+     * @covers \Redmine\Psr18Client
+     * @test
+     *
+     * @dataProvider createdGetRequestsData
+     */
+    public function testCreateRequestsContainsRelevantData($path, $expectedOutput)
+    {
+        $response = $this->createMock(ResponseInterface::class);
+
+        $httpClient = $this->createMock(HttpClient::class);
+        $httpClient->method('sendRequest')->will(
+            $this->returnCallback(function($request) use ($response, $expectedOutput) {
+                $content = $request->getBody()->__toString();
+
+                $cookieHeader = '';
+                $cookies = [];
+
+                foreach ($request->getCookieParams() as $k => $v) {
+                    $cookies[] = $k.'='.$v;
+                }
+
+                $headers = '';
+
+                foreach ($request->getHeaders() as $k => $v) {
+                    $headers .= $k . ": " . $request->getHeaderLine($k)."\r\n";
+                }
+
+                if (!empty($cookies)) {
+                    $cookieHeader = 'Cookie: '.implode('; ', $cookies)."\r\n";
+                }
+
+                $fullRequest = sprintf(
+                    '%s %s %s',
+                    $request->getMethod(),
+                    $request->getUri()->__toString(),
+                    $request->getProtocolVersion()
+                )."\r\n".
+                    $headers.
+                    $cookieHeader."\r\n".
+                    $content
+                ;
+
+                $this->assertSame($expectedOutput, $fullRequest);
+
+                return $response;
+            })
+        );
+
+        $requestFactory = $this->createMock(ServerRequestFactoryInterface::class);
+        $requestFactory->method('createServerRequest')->will(
+            $this->returnCallback(function($method, $uri) {
+                return new ServerRequest($method, $uri);
+            })
+        );
+
+        $client = new Psr18Client(
+            $httpClient,
+            $requestFactory,
+            $this->createMock(StreamFactoryInterface::class),
+            'http://test.local',
+            'access_token'
+        );
+
+        $client->requestGet($path);
+    }
+
+    public function createdGetRequestsData()
+    {
+        return [
+            ['/path', "GET http://test.local/path 1.1\r\nHost: test.local\r\n\r\n"],
+            ['/path.json', "GET http://test.local/path.json 1.1\r\nHost: test.local\r\n\r\n"],
+        ];
     }
 }
