09bd96d58aad7dc35f0cb36484bab4b14c377720
Art4 <art4@wlabs.de>
2021-12-14 10:17:38 +0100

    Add new ClientException and Exception interface

---
+++ src/Redmine/Exception.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Redmine;
+
+use Throwable;
+
+/**
+ * Exception interface.
+ */
+interface Exception extends Throwable
+{
+}
---
+++ src/Redmine/Exception/ClientException.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Redmine\Exception;
+
+use Exception;
+use Redmine\Exception as RedmineException;
+
+/**
+ * ClientException.
+ */
+class ClientException extends Exception implements RedmineException
+{
+}
