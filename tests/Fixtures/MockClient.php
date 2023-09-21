a1977209d2f0953d838d0f9a9f73cb50f07f10a4
Art4 <art4@wlabs.de>
2023-09-21 12:57:49 +0200

    Fix method name in MockClient

--- tests/Fixtures/MockClient.php
+++ tests/Fixtures/MockClient.php
@@ -39,7 +39,7 @@ final class MockClient implements Client
     public $responseCodeMock;
     public $responseContentTypeMock;
 
-    private function __csontruct() {
+    private function __construct() {
     }
 
     /**
