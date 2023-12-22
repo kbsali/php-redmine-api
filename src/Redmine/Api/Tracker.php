e458e976579c74819a9be3929c9120f45d131b80
Art4 <art4@wlabs.de>
2023-12-22 09:36:55 +0100

    Restore BC in Tracker::all()

--- src/Redmine/Api/Tracker.php
+++ src/Redmine/Api/Tracker.php
@@ -2,6 +2,8 @@
 
 namespace Redmine\Api;
 
+use Redmine\Exception;
+
 /**
  * Listing trackers.
  *
@@ -38,13 +40,21 @@ class Tracker extends AbstractApi
      *
      * @param array $params optional parameters to be passed to the api (offset, limit, ...)
      *
-     * @return array list of trackers found
+     * @return array|string|false list of trackers found or error message or false
      */
     public function all(array $params = [])
     {
         @trigger_error('`'.__METHOD__.'()` is deprecated since v2.4.0, use `'.__CLASS__.'::list()` instead.', E_USER_DEPRECATED);
 
-        return $this->list($params);
+        try {
+            return $this->list($params);
+        } catch (Exception $e) {
+            if ($this->client->getLastResponseBody() === '') {
+                return false;
+            }
+
+            return $e->getMessage();
+        }
     }
 
     /**
