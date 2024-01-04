bc105b30b33c6017787534383ab8b2fee64369c9
Art4 <art4@wlabs.de>
2024-01-04 16:56:44 +0100

    Allow all HTTP methods in HttpClient

--- src/Redmine/Http/HttpClient.php
+++ src/Redmine/Http/HttpClient.php
@@ -5,28 +5,23 @@ declare(strict_types=1);
 namespace Redmine\Http;
 
 use Redmine\Exception\ClientException;
-use Redmine\Exception\InvalidHttpMethodException;
 
 /**
- * client interface.
+ * Minimalistic HTTP client interface.
+ *
+ * The interface is designed in such a way that it is prepared for all future Redmine features.
+ * Therefore, it does not specify or restrict the HTTP method, the URL, headers or the body.
+ *
+ * The client is responsible for ensuring that all data is sent in the correct form and
+ * that received data is processed correctly.
  */
 interface HttpClient
 {
-    public const GET = 'GET';
-
-    public const POST = 'POST';
-
-    public const PUT = 'PUT';
-
-    public const DELETE = 'DELETE';
-
     /**
      * Create and send a HTTP request and return the response
      *
-     * @param self::* $method Valid values are 'GET', 'POST', 'PUT' and 'DELETE'
      * @param string $body must be empty string on 'GET' request
      *
-     * @throws InvalidHttpMethodException if $method is not a valid value
      * @throws ClientException If anything goes wrong on creating or sending the request
      */
     public function request(string $method, string $path, string $body = ''): Response;
