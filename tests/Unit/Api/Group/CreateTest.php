eee0290ac4a0a033484aa1b68bc1c1a6c5b29922
Art4 <art4@wlabs.de>
2024-01-10 16:19:21 +0100

    Add test to create group with custom fields

--- tests/Unit/Api/Group/CreateTest.php
+++ tests/Unit/Api/Group/CreateTest.php
@@ -80,6 +80,43 @@ class CreateTest extends TestCase
         );
     }
 
+    public function testCreateWithNameAndCustomFieldsCreatesGroup()
+    {
+        $client = $this->createMock(HttpClient::class);
+        $client->expects($this->exactly(1))
+            ->method('request')
+            ->willReturnCallback(function (string $method, string $path, string $body = '') {
+                $this->assertSame('POST', $method);
+                $this->assertSame('/groups.xml', $path);
+                $this->assertXmlStringEqualsXmlString('<?xml version="1.0"?><group><name>Group Name</name><custom_fields type="array"><custom_field id="1"><value>5</value></custom_field></custom_fields></group>', $body);
+
+                return $this->createConfiguredMock(
+                    Response::class,
+                    [
+                        'getContentType' => 'application/xml',
+                        'getBody' => '<?xml version="1.0"?><group></group>',
+                    ]
+                );
+            });
+
+        // Create the object under test
+        $api = new Group($client);
+
+        // Perform the tests
+        $xmlElement = $api->create([
+            'name' => 'Group Name',
+            'custom_fields' => [
+                ['id' => 1, 'value' => 5],
+            ],
+        ]);
+
+        $this->assertInstanceOf(SimpleXMLElement::class, $xmlElement);
+        $this->assertXmlStringEqualsXmlString(
+            '<?xml version="1.0"?><group></group>',
+            $xmlElement->asXml(),
+        );
+    }
+
     /**
      * @covers ::create
      */
