10260fb35e37275c5fce75fd2eaaad42fa4dfd2c
Art4 <art4@wlabs.de>
2024-01-10 16:55:22 +0100

    Fix tests

--- tests/Integration/GroupXmlTest.php
+++ tests/Integration/GroupXmlTest.php
@@ -2,7 +2,6 @@
 
 namespace Redmine\Tests\Integration;
 
-use Exception;
 use PHPUnit\Framework\TestCase;
 use Redmine\Exception\MissingParameterException;
 use Redmine\Tests\Fixtures\MockClient;
@@ -48,15 +47,30 @@ class GroupXmlTest extends TestCase
         );
     }
 
-    public function testUpdateNotImplemented()
+    public function testUpdateComplex()
     {
         /** @var \Redmine\Api\Group */
         $api = MockClient::create()->getApi('group');
-        $this->assertInstanceOf('Redmine\Api\Group', $api);
+        $res = $api->update(5, [
+            'name' => 'Developers',
+            'user_ids' => [3, 5],
+        ]);
+        $response = json_decode($res, true);
 
-        $this->expectException(Exception::class);
-        $this->expectExceptionMessage('Not implemented');
-
-        $api->update(1);
+        $this->assertEquals('PUT', $response['method']);
+        $this->assertEquals('/groups/5.xml', $response['path']);
+        $this->assertXmlStringEqualsXmlString(
+            <<< XML
+            <?xml version="1.0"?>
+            <group>
+                <name>Developers</name>
+                <user_ids type="array">
+                    <user_id>3</user_id>
+                    <user_id>5</user_id>
+                </user_ids>
+            </group>
+            XML,
+            $response['data']
+        );
     }
 }
