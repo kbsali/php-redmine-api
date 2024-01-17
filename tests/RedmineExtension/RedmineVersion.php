b9e3d807ed348eb981940924fed88dc15c6305c4
Art4 <art4@wlabs.de>
2024-01-17 10:36:03 +0100

    Change RedmineVersion to string baked Enum

--- tests/RedmineExtension/RedmineVersion.php
+++ tests/RedmineExtension/RedmineVersion.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Redmine\Tests\RedmineExtension;
 
-enum RedmineVersion: int
+enum RedmineVersion: string
 {
     /**
      * Redmine 5.1.1
@@ -12,7 +12,7 @@ enum RedmineVersion: int
      * @link https://www.redmine.org/versions/191
      * @link https://www.redmine.org/projects/redmine/wiki/Changelog_5_1#511-2023-11-27
      */
-    case V5_1_1 = 50101;
+    case V5_1_1 = '5.1.1';
 
     /**
      * Redmine 5.1.0
@@ -20,7 +20,7 @@ enum RedmineVersion: int
      * @link https://www.redmine.org/versions/176
      * @link https://www.redmine.org/projects/redmine/wiki/Changelog_5_1#510-2023-10-31
      */
-    case V5_1_0 = 50100;
+    case V5_1_0 = '5.1.0';
 
     /**
      * Redmine 5.0.7
@@ -28,19 +28,20 @@ enum RedmineVersion: int
      * @link https://www.redmine.org/versions/189
      * @link https://www.redmine.org/projects/redmine/wiki/Changelog_5_0#507-2023-11-27
      */
-    case V5_0_7 = 50007;
+    case V5_0_7 = '5.0.7';
 
     public function asString(): string
     {
-        return match($this) {
-            RedmineVersion::V5_0_7 => '5.0.7',
-            RedmineVersion::V5_1_0 => '5.1.0',
-            RedmineVersion::V5_1_1 => '5.1.1',
-        };
+        return $this->value;
     }
 
+    /**
+     * returns the version as integer ID, e.g. `50101`
+     */
     public function asId(): int
     {
-        return $this->value;
+        $parts = explode('.', $this->value);
+
+        return intval($parts[0]) * 10000 + intval($parts[1]) * 100 + intval($parts[2]);
     }
 }
