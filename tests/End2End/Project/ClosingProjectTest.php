5ac0f5e6e2781c2b43b7a742258879b8a931347b
Art4 <art4@wlabs.de>
2024-01-18 08:55:33 +0100

    Create e2e test for closing a project

---
+++ tests/End2End/Project/ClosingProjectTest.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Redmine\Tests\End2End\Project;
+
+use DateTimeImmutable;
+use Redmine\Api\Project;
+use Redmine\Tests\End2End\ClientTestCase;
+use Redmine\Tests\RedmineExtension\RedmineVersion;
+
+class ClosingProjectTest extends ClientTestCase
+{
+    /**
+     * @dataProvider provideRedmineVersions
+     */
+    public function testInteractionWithProject(RedmineVersion $redmineVersion): void
+    {
+        $client = $this->getNativeCurlClient($redmineVersion);
+
+        /** @var Project */
+        $api = $client->getApi('project');
+        $now = new DateTimeImmutable();
+
+        // Create project
+        $projectName = 'test project';
+        $projectIdentifier = 'test_project';
+
+        $xmlData = $api->create([
+            'name' => $projectName,
+            'identifier' => $projectIdentifier,
+        ]);
+
+        $projectDataJson = json_encode($xmlData);
+        $projectData = json_decode($projectDataJson, true);
+
+        $this->assertIsArray($projectData, $projectDataJson);
+        $this->assertArrayHasKey('identifier', $projectData, $projectDataJson);
+        $this->assertSame($projectIdentifier, $projectData['identifier'], $projectDataJson);
+        $this->assertArrayHasKey('status', $projectData, $projectDataJson);
+        $this->assertSame('1', $projectData['status'], $projectDataJson);
+
+        // Close project
+        $result = $api->update($projectIdentifier, [
+            'status' => 5,
+        ]);
+        $this->assertSame('', $result);
+
+        // Read single project
+        $projectDetails = $api->show($projectIdentifier);
+
+        $this->assertArrayHasKey('project', $projectDetails);
+        $this->assertArrayHasKey('status', $projectDetails['project']);
+        $this->assertSame('5', $projectDetails['project']['status']);
+    }
+}
