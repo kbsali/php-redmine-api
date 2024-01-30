93cef18bd87e4770bef516044d849ba05c4f7ca7
Art4 <art4@wlabs.de>
2024-01-30 14:51:55 +0100

    Add method UnexpectedResponseException::getResponse()

--- src/Redmine/Exception/UnexpectedResponseException.php
+++ src/Redmine/Exception/UnexpectedResponseException.php
@@ -1,9 +1,13 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Redmine\Exception;
 
 use Redmine\Exception as RedmineException;
+use Redmine\Http\Response;
 use RuntimeException;
+use Throwable;
 
 /**
  * Exception if the Redmine server delivers an unexpected response.
@@ -14,4 +18,28 @@ use RuntimeException;
  * - Redmine\Client\Client::getLastResponseContentType()
  * - Redmine\Client\Client::getLastResponseBody()
  */
-final class UnexpectedResponseException extends RuntimeException implements RedmineException {}
+final class UnexpectedResponseException extends RuntimeException implements RedmineException
+{
+    /**
+     * @var Response|null
+     */
+    private $response = null;
+
+    public static function create(Response $response, Throwable $prev = null): self
+    {
+        $e = new self(
+            'The Redmine server responded with an unexpected body.',
+            ($prev !== null) ? $prev->getCode() : 1,
+            $prev
+        );
+
+        $e->response = $response;
+
+        return $e;
+    }
+
+    public function getResponse(): ?Response
+    {
+        return $this->response;
+    }
+}
---
+++ tests/Unit/Exception/UnexpectedResponseExceptionTest.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Redmine\Tests\Unit\Exception;
+
+use Exception;
+use PHPUnit\Framework\TestCase;
+use Redmine\Exception as RedmineException;
+use Redmine\Exception\UnexpectedResponseException;
+use Redmine\Http\Response;
+
+class UnexpectedResponseExceptionTest extends TestCase
+{
+    public function testCreateReturnsException()
+    {
+        $response = $this->createMock(Response::class);
+
+        $exception = UnexpectedResponseException::create($response);
+
+        $this->assertInstanceOf(Exception::class, $exception);
+        $this->assertInstanceOf(RedmineException::class, $exception);
+    }
+
+    public function testCreateWithThrowable()
+    {
+        $response = $this->createMock(Response::class);
+        $throwable = new Exception('message', 5);
+
+        $exception = UnexpectedResponseException::create($response, $throwable);
+
+        $this->assertSame(5, $exception->getCode());
+        $this->assertSame($throwable, $exception->getPrevious());
+    }
+
+    public function testGetResponseReturnsResponse()
+    {
+        $response = $this->createMock(Response::class);
+
+        $exception = UnexpectedResponseException::create($response);
+
+        $this->assertSame($response, $exception->getResponse());
+        $this->assertInstanceOf(RedmineException::class, $exception);
+    }
+}
