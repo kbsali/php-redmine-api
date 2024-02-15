3b31ea671212d9e1e92a0daf9c4c68dc711d3de0
Art4 <art4@wlabs.de>
2024-02-15 10:02:20 +0100

    improve Wiki::show() tests

--- src/Redmine/Api/Wiki.php
+++ src/Redmine/Api/Wiki.php
@@ -114,15 +114,15 @@ class Wiki extends AbstractApi
 
         $body = $this->lastResponse->getContent();
 
-        if ('' !== $body) {
-            try {
-                return JsonSerializer::createFromString($body)->getNormalized();
-            } catch (SerializerException $e) {
-                return 'Error decoding body as JSON: ' . $e->getPrevious()->getMessage();
-            }
+        if ('' === $body) {
+            return false;
         }
 
-        return false;
+        try {
+            return JsonSerializer::createFromString($body)->getNormalized();
+        } catch (SerializerException $e) {
+            return 'Error decoding body as JSON: ' . $e->getPrevious()->getMessage();
+        }
     }
 
     /**
