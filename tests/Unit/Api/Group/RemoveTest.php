e7a5cc0e21414bcd252f6a3003f6f9cc19acacfc
Art4 <art4@wlabs.de>
2024-03-14 16:10:10 +0100

    Improve tests for Group::removeUser() and Group::remove()

---
+++ tests/Unit/Api/Group/RemoveTest.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Redmine\Tests\Unit\Api\Group;
+
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\TestCase;
+use Redmine\Api\Group;
+use Redmine\Tests\Fixtures\AssertingHttpClient;
+
+#[CoversClass(Group::class)]
+class RemoveTest extends TestCase
+{
+    public function testRemoveReturnsString()
+    {
+        $client = AssertingHttpClient::create(
+            $this,
+            [
+                'DELETE',
+                '/groups/5.xml',
+                'application/xml',
+                '',
+                204,
+                '',
+                ''
+            ]
+        );
+
+        $api = new Group($client);
+
+        $this->assertSame('', $api->remove(5));
+    }
+}
---
+++ tests/Unit/Api/Group/RemoveUserTest.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Redmine\Tests\Unit\Api\Group;
+
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\TestCase;
+use Redmine\Api\Group;
+use Redmine\Tests\Fixtures\AssertingHttpClient;
+
+#[CoversClass(Group::class)]
+class RemoveUserTest extends TestCase
+{
+    public function testRemoveUserReturnsString()
+    {
+        $client = AssertingHttpClient::create(
+            $this,
+            [
+                'DELETE',
+                '/groups/5/users/10.xml',
+                'application/xml',
+                '',
+                204,
+                '',
+                ''
+            ]
+        );
+
+        $api = new Group($client);
+
+        $this->assertSame('', $api->removeUser(5, 10));
+    }
+}
