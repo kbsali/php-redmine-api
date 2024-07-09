60d4b07408693c590f22f4fdd48d54b5195fb022
Art4 <art4@wlabs.de>
2024-07-09 12:46:25 +0200

    Extract TimeEntryActivity::listing()

--- src/Redmine/Api/TimeEntryActivity.php
+++ src/Redmine/Api/TimeEntryActivity.php
@@ -105,15 +105,7 @@ class TimeEntryActivity extends AbstractApi
     {
         @trigger_error('`' . __METHOD__ . '()` is deprecated since v2.7.0, use `' . __CLASS__ . '::listNames()` instead.', E_USER_DEPRECATED);
 
-        if (empty($this->timeEntryActivities) || $forceUpdate) {
-            $this->timeEntryActivities = $this->list();
-        }
-        $ret = [];
-        foreach ($this->timeEntryActivities['time_entry_activities'] as $e) {
-            $ret[$e['name']] = (int) $e['id'];
-        }
-
-        return $ret;
+        return $this->doListing((bool) $forceUpdate);
     }
 
     /**
@@ -125,11 +117,27 @@ class TimeEntryActivity extends AbstractApi
      */
     public function getIdByName($name)
     {
-        $arr = $this->listing();
+        $arr = $this->doListing(false);
+
         if (!isset($arr[$name])) {
             return false;
         }
 
         return $arr[(string) $name];
+    }
+
+    private function doListing(bool $forceUpdate)
+    {
+        if (empty($this->timeEntryActivities) || $forceUpdate) {
+            $this->timeEntryActivities = $this->list();
+        }
+
+        $ret = [];
+
+        foreach ($this->timeEntryActivities['time_entry_activities'] as $e) {
+            $ret[$e['name']] = (int) $e['id'];
+        }
+
+        return $ret;
     }
 }
