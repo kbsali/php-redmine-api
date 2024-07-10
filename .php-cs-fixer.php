e069bf527772effa2dedc25e99a1de800921f6bd
Art4 <art4@wlabs.de>
2024-07-10 08:52:33 +0200

    Run PHP-CS-Fixer with ruleset PHPUnit100Migration:risky

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -11,7 +11,7 @@ $config = new PhpCsFixer\Config();
 return $config->setRules([
         '@PER-CS2.0' => true,
         '@PER-CS2.0:risky' => true,
-        '@PHPUnit84Migration:risky' => true,
+        '@PHPUnit100Migration:risky' => true,
         'array_syntax' => ['syntax' => 'short'],
         'linebreak_after_opening_tag' => true,
         'ordered_imports' => true,
