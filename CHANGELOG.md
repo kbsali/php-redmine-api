0a946e6b1d8d0ca5cfc3ef568cbbf2346988d6b6
Art4 <art4@wlabs.de>
2026-02-11 06:33:41 +0100

    feat:create IssueStatus::fromHttpClient()

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -16,6 +16,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 - New method `Redmine\Api\IssueCategory::fromHttpClient()` for creating the class.
 - New method `Redmine\Api\IssuePriority::fromHttpClient()` for creating the class.
 - New method `Redmine\Api\IssueRelation::fromHttpClient()` for creating the class.
+- New method `Redmine\Api\IssueStatus::fromHttpClient()` for creating the class.
 - Add support for PHP 8.5
 - Add support for Redmine 6.1.
 
@@ -39,6 +40,8 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 - Extending `Redmine\Api\IssuePriority` is deprecated and will be set to final in future, create a wrapper class instead.
 - `Redmine\Api\IssueRelation::__construct()` is deprecated and will be set to private in future, use `\Redmine\Api\IssueRelation::fromHttpClient()` instead.
 - Extending `Redmine\Api\IssueRelation` is deprecated and will be set to final in future, create a wrapper class instead.
+- `Redmine\Api\IssueStatus::__construct()` is deprecated and will be set to private in future, use `\Redmine\Api\IssueStatus::fromHttpClient()` instead.
+- Extending `Redmine\Api\IssueStatus` is deprecated and will be set to final in future, create a wrapper class instead.
 
 ### Removed
 
--- tests/Unit/Api/IssueStatusTest.php
+++ tests/Unit/Api/IssueStatusTest.php
@@ -7,6 +7,7 @@ use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
 use Redmine\Api\IssueStatus;
 use Redmine\Client\Client;
+use Redmine\Http\HttpClient;
 use Redmine\Tests\Fixtures\MockClient;
 
 /**
@@ -15,6 +16,44 @@ use Redmine\Tests\Fixtures\MockClient;
 #[CoversClass(IssueStatus::class)]
 class IssueStatusTest extends TestCase
 {
+    public function testExtendingTheClassTriggersDeprecationWarning(): void
+    {
+        // PHPUnit 10 compatible way to test trigger_error().
+        set_error_handler(
+            function ($errno, $errstr): bool {
+                $this->assertSame(
+                    'Class `Redmine\Api\IssueStatus` will declared as final in v3.0.0, stop extending it.',
+                    $errstr,
+                );
+
+                restore_error_handler();
+                return true;
+            },
+            E_USER_DEPRECATED,
+        );
+
+        new class ($this->createStub(HttpClient::class)) extends IssueStatus {};
+    }
+
+    public function testConstructorTriggersDeprecationWarning(): void
+    {
+        // PHPUnit 10 compatible way to test trigger_error().
+        set_error_handler(
+            function ($errno, $errstr): bool {
+                $this->assertSame(
+                    'Method `Redmine\Api\IssueStatus::__construct()` is deprecated since v2.9.0 and will declared as private in v3.0.0, use `Redmine\Api\IssueStatus::fromHttpClient()` instead.',
+                    $errstr,
+                );
+
+                restore_error_handler();
+                return true;
+            },
+            E_USER_DEPRECATED,
+        );
+
+        new IssueStatus($this->createStub(HttpClient::class));
+    }
+
     /**
      * Test all().
      */
