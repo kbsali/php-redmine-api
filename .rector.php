c030d23f35602107c8e10310a90d7ece0dae74e3
Art4 <art4@wlabs.de>
2026-07-25 20:52:29 +0200

    chore: update dependencies
    - Update guzzlehttp/psr7 constraint to ^2.8 || ^3.0
    - Update friendsofphp/php-cs-fixer constraint to ^3.95
    - Update phpstan/phpstan constraint to ^2.2
    - Update rector/rector constraint to ^2.5
    - Skip newly added type-declaration rules in .rector.php to avoid breaking changes in src

--- .rector.php
+++ .rector.php
@@ -21,4 +21,10 @@ return \Rector\Config\RectorConfig::configure()
         \Rector\Set\ValueObject\SetList::STRICT_BOOLEANS,
         \Rector\Set\ValueObject\SetList::PRIVATIZATION,
     ])
+    ->withSkip([
+        \Rector\TypeDeclaration\Rector\ClassMethod\ScalarParamTypeByMethodCallTypeRector::class,
+        \Rector\TypeDeclaration\Rector\ClassMethod\ArrayParamTypeByMethodCallTypeRector::class,
+        \Rector\CodeQuality\Rector\BooleanNot\SimplifyDeMorganBinaryRector::class,
+        \Rector\CodeQuality\Rector\BooleanNot\NegatedAndsToPositiveOrsRector::class,
+    ])
 ;
