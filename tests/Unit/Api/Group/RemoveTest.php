8ce91bbbfd42c7a05f7126a97e5785ea7ad5a673
Art4 <art4@wlabs.de>
2026-07-26 15:29:24 +0200

    feat: check for correct response status code on Group::remove()

--- tests/Unit/Api/Group/RemoveTest.php
+++ tests/Unit/Api/Group/RemoveTest.php
@@ -7,6 +7,8 @@ namespace Redmine\Tests\Unit\Api\Group;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\TestCase;
 use Redmine\Api\Group;
+use Redmine\Exception\UnexpectedResponseException;
+use Redmine\Future;
 use Redmine\Tests\Fixtures\AssertingHttpClient;
 
 #[CoversClass(Group::class)]
@@ -30,5 +32,53 @@ class RemoveTest extends TestCase
         $api = Group::fromHttpClient($client);
 
         $this->assertSame('', $api->remove(5));
+    }
+
+    public function testRemoveWithIncorrectStatusCodeReturnsBody(): void
+    {
+        $client = AssertingHttpClient::create(
+            $this,
+            [
+                'DELETE',
+                '/groups/5.xml',
+                'application/xml',
+                '',
+                500,
+                '',
+                '',
+            ],
+        );
+
+        $api = Group::fromHttpClient($client);
+
+        $this->assertSame('', $api->remove(5));
+    }
+
+    public function testRemoveWithIncorrectStatusCodeThrowsException(): void
+    {
+        $client = AssertingHttpClient::create(
+            $this,
+            [
+                'DELETE',
+                '/groups/5.xml',
+                'application/xml',
+                '',
+                500,
+                '',
+                '',
+            ],
+        );
+
+        $api = Group::fromHttpClient($client);
+
+        $this->expectException(UnexpectedResponseException::class);
+
+        try {
+            Future::enableForwardCompatibility();
+
+            $api->remove(5);
+        } finally {
+            Future::disableForwardCompatibility();
+        }
     }
 }
