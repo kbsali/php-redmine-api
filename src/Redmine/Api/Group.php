12be2de1c6fddbee3e95a35477237c20dd73fa80
Art4 <art4@wlabs.de>
2026-07-26 15:32:44 +0200

    feat: check for correct response status code on Group::removeUser()

--- src/Redmine/Api/Group.php
+++ src/Redmine/Api/Group.php
@@ -368,6 +368,17 @@ class Group extends AbstractApi
             '/groups/' . strval($id) . '/users/' . strval($userId) . '.xml',
         ));
 
-        return $this->lastResponse->getContent();
+        $body = $this->lastResponse->getContent();
+        $statusCode = $this->lastResponse->getStatusCode();
+
+        if ($statusCode !== 200 && $statusCode !== 204) {
+            if (!Future::isForwardCompatibilityEnabled()) {
+                return $body;
+            }
+
+            throw UnexpectedResponseException::create($this->lastResponse);
+        }
+
+        return $body;
     }
 }
--- tests/Unit/Api/Group/RemoveUserTest.php
+++ tests/Unit/Api/Group/RemoveUserTest.php
@@ -7,6 +7,8 @@ namespace Redmine\Tests\Unit\Api\Group;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\TestCase;
 use Redmine\Api\Group;
+use Redmine\Exception\UnexpectedResponseException;
+use Redmine\Future;
 use Redmine\Tests\Fixtures\AssertingHttpClient;
 
 #[CoversClass(Group::class)]
@@ -30,5 +32,53 @@ class RemoveUserTest extends TestCase
         $api = Group::fromHttpClient($client);
 
         $this->assertSame('', $api->removeUser(5, 10));
+    }
+
+    public function testRemoveUserWithIncorrectStatusCodeReturnsBody(): void
+    {
+        $client = AssertingHttpClient::create(
+            $this,
+            [
+                'DELETE',
+                '/groups/5/users/10.xml',
+                'application/xml',
+                '',
+                500,
+                '',
+                '',
+            ],
+        );
+
+        $api = Group::fromHttpClient($client);
+
+        $this->assertSame('', $api->removeUser(5, 10));
+    }
+
+    public function testRemoveUserWithIncorrectStatusCodeThrowsException(): void
+    {
+        $client = AssertingHttpClient::create(
+            $this,
+            [
+                'DELETE',
+                '/groups/5/users/10.xml',
+                'application/xml',
+                '',
+                500,
+                '',
+                '',
+            ],
+        );
+
+        $api = Group::fromHttpClient($client);
+
+        $this->expectException(UnexpectedResponseException::class);
+
+        try {
+            Future::enableForwardCompatibility();
+
+            $api->removeUser(5, 10);
+        } finally {
+            Future::disableForwardCompatibility();
+        }
     }
 }
