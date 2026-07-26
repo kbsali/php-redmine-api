fb7dbfc3a3a6d453b5cf9d5a025250559f39911e
Art4 <art4@wlabs.de>
2026-07-26 14:29:11 +0200

    feat: check for correct response status code on Attachment::remove()

--- src/Redmine/Api/Attachment.php
+++ src/Redmine/Api/Attachment.php
@@ -186,6 +186,17 @@ class Attachment extends AbstractApi
             '/attachments/' . urlencode(strval($id)) . '.xml',
         ));
 
-        return $this->lastResponse->getContent();
+        $body = $this->lastResponse->getContent();
+        $statusCode = $this->lastResponse->getStatusCode();
+
+        if ($statusCode !== 200 && $statusCode !== 204) {
+            if (!Future::isForwardCompatibilityEnabled()) {
+                return $body;
+            }
+
+            throw UnexpectedResponseException::create($this->lastResponse);
+        }
+
+        return $body;
     }
 }
--- tests/Unit/Api/Attachment/RemoveTest.php
+++ tests/Unit/Api/Attachment/RemoveTest.php
@@ -7,6 +7,8 @@ namespace Redmine\Tests\Unit\Api\Attachment;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\TestCase;
 use Redmine\Api\Attachment;
+use Redmine\Exception\UnexpectedResponseException;
+use Redmine\Future;
 use Redmine\Tests\Fixtures\AssertingHttpClient;
 
 #[CoversClass(Attachment::class)]
@@ -30,5 +32,53 @@ class RemoveTest extends TestCase
         $api = Attachment::fromHttpClient($client);
 
         $this->assertSame('', $api->remove(5));
+    }
+
+    public function testRemoveWithIncorrectStatusCodeReturnsBody(): void
+    {
+        $client = AssertingHttpClient::create(
+            $this,
+            [
+                'DELETE',
+                '/attachments/5.xml',
+                'application/xml',
+                '',
+                500,
+                '',
+                'error body',
+            ],
+        );
+
+        $api = Attachment::fromHttpClient($client);
+
+        $this->assertSame('error body', $api->remove(5));
+    }
+
+    public function testRemoveWithIncorrectStatusCodeThrowsException(): void
+    {
+        $client = AssertingHttpClient::create(
+            $this,
+            [
+                'DELETE',
+                '/attachments/5.xml',
+                'application/xml',
+                '',
+                500,
+                '',
+                '',
+            ],
+        );
+
+        $api = Attachment::fromHttpClient($client);
+
+        $this->expectException(UnexpectedResponseException::class);
+
+        try {
+            Future::enableForwardCompatibility();
+
+            $api->remove(5);
+        } finally {
+            Future::disableForwardCompatibility();
+        }
     }
 }
