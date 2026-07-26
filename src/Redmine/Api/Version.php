b12eb352bec0e6db4cd978ec0901b1ad861115dd
Art4 <art4@wlabs.de>
2026-07-26 15:23:13 +0200

    feat: check for correct response status code on Version::update()

--- src/Redmine/Api/Version.php
+++ src/Redmine/Api/Version.php
@@ -321,7 +321,18 @@ class Version extends AbstractApi
             XmlSerializer::createFromArray(['version' => $params])->getEncoded(),
         ));
 
-        return $this->lastResponse->getContent();
+        $body = $this->lastResponse->getContent();
+        $statusCode = $this->lastResponse->getStatusCode();
+
+        if ($statusCode !== 200 && $statusCode !== 204) {
+            if (!Future::isForwardCompatibilityEnabled()) {
+                return $body;
+            }
+
+            throw UnexpectedResponseException::create($this->lastResponse);
+        }
+
+        return $body;
     }
 
     /**
