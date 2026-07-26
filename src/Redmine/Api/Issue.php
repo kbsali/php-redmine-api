42bb0fe989aea6879d155a2794858a3abff88efd
Art4 <art4@wlabs.de>
2026-07-26 15:53:26 +0200

    feat: check for correct response status code on Issue::removeWatcher()

--- src/Redmine/Api/Issue.php
+++ src/Redmine/Api/Issue.php
@@ -370,7 +370,18 @@ class Issue extends AbstractApi
             '/issues/' . urlencode(strval($id)) . '/watchers/' . urlencode(strval($watcherUserId)) . '.xml',
         ));
 
-        return $this->lastResponse->getContent();
+        $body = $this->lastResponse->getContent();
+        $statusCode = $this->lastResponse->getStatusCode();
+
+        if ($statusCode !== 200 && $statusCode !== 204) {
+            if (!Future::isForwardCompatibilityEnabled()) {
+                return $body;
+            }
+
+            throw UnexpectedResponseException::create($this->lastResponse);
+        }
+
+        return $body;
     }
 
     /**
